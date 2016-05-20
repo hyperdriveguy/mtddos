1c12d67a0a236d3b2672bc0121bb7d12750049e6
hyperdriveguy <hyperdriveguy@users.noreply.github.com>
2016-05-20 13:55:06 -0700

    Change required privledge

--- init.lua
+++ init.lua
@@ -1,14 +1,9 @@
-
-
-
-
-
 minetest.register_chatcommand("restart", {
     params = "";
-    description = "Restart game.";
-    privs = { privs=true; };
+    description = "Restart Minetest.";
+    privs = { server=true; };
     func = function ( name, param )
-	os.execute(minetest.get_modpath("restart").."/restart &")
+	os.execute(minetest.get_modpath("mtddos").."/restart &")
          minetest.request_shutdown()
     end;
 });
