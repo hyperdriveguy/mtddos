7094ecf3f1aaa8dcec51b3bedfc776cd17e09078
hyperdriveguy <hyperdriveguy@users.noreply.github.com>
2016-05-20 14:45:46 -0700

    Add reboot command

--- init.lua
+++ init.lua
@@ -1,9 +1,17 @@
-minetest.register_chatcommand("restart", {
+minetest.register_chatcommand("mtrestart", {
     params = "";
     description = "Restart Minetest.";
     privs = { server=true; };
     func = function ( name, param )
-	os.execute(minetest.get_modpath("mtddos").."/restart &")
+	os.execute(minetest.get_modpath("mtddos").."/mtrestart &")
          minetest.request_shutdown()
+    end;
+});
+minetest.register_chatcommand("reboot", {
+    params = "[time]";
+    description = "Restart the server.";
+    privs = { server=true; };
+    func = function ( name, param )
+	os.execute(minetest.get_modpath("mtddos").."/server_restart")
     end;
 });
