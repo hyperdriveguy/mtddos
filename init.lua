261ef657eb686410c0330d23b27266982ed21ddb
hyperdriveguy <hyperdriveguy@users.noreply.github.com>
2016-05-20 15:07:43 -0700

    Final changes

--- init.lua
+++ init.lua
@@ -1,17 +1,18 @@
-minetest.register_chatcommand("mtrestart", {
+minetest.register_chatcommand("serv_shutdown", {
     params = "";
-    description = "Restart Minetest.";
+    description = "Shut down the server.";
     privs = { server=true; };
     func = function ( name, param )
-	os.execute(minetest.get_modpath("mtddos").."/mtrestart &")
-         minetest.request_shutdown()
+        minetest.request_shutdown()
+	os.execute(minetest.get_modpath("mtddos").."/server_shutdown")
     end;
 });
-minetest.register_chatcommand("reboot", {
+minetest.register_chatcommand("serv_reboot", {
     params = "";
     description = "Restart the server.";
     privs = { server=true; };
     func = function ( name, param )
+        minetest.request_shutdown()
 	os.execute(minetest.get_modpath("mtddos").."/server_restart")
     end;
 });
